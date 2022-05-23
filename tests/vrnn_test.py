a685ff1d5bf6c44e8a1d739f6ab3148358792a84
mc821@ic.ac.uk <mc821@ic.ac.uk>
2022-05-24 00:27:00 +0100

    Edited tests and new plots

--- tests/vrnn_test.py
+++ tests/vrnn_test.py
@@ -1,6 +1,6 @@
 import torch
 
-from model import VRNN
+from model_vrnn import VRNN
 from data.MovingMNIST import MovingMNIST
 import torch.nn as nn
 
@@ -16,7 +16,7 @@ train_loader = torch.utils.data.DataLoader(
 
 model = VRNN(64, 1024, 32, 1)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-mse_loss = nn.MSELoss()
+mse_loss = nn.MSELoss() # by default, mean --- average over all pixels and time steps
 
 for data, _ in train_loader:
     data = torch.unsqueeze(data, 2) # Batch Size X Seq Length X Channels X Height X Width
@@ -25,32 +25,33 @@ for data, _ in train_loader:
     # print(torch.min(data)) # 0
 
     optimizer.zero_grad()
-    kld_loss, nll_loss, _ = model(data)
+    kld_loss, recon_loss, _ = model(data)
 
     # Normalise reconstruction loss by number of pixels and sequence length
-    print("MSE before normalisation", nll_loss)
-    nll_loss = nll_loss / (64 * 64 * 10)
-    print("MSE after normalisation", nll_loss) # 6.2398e-05
+    print("MSE before normalisation", recon_loss)
+    recon_loss = recon_loss / (64 * 64 * 10)
+    print("Pixel MSE for 1 time step for random model", recon_loss) # 6.2398e-05
 
     # print out black image at all times
     black_seq = torch.full_like(data, 0)
     black_mse = mse_loss(black_seq, data)
 
-    print("MSE of black image before normalisation", black_mse * 10)
-    black_mse = black_mse/(64 * 64 * 10)
-    print("MSE of black image after normalisation", black_mse) # 1.0636e-06 - low because most of MNIST is black
+    print("Pixel MSE of black image for 1 time step", black_mse)
+
+    # print("MSE of black image before normalisation", black_mse * 10)
+    # black_mse = black_mse/(64 * 64 * 10)
+    # print("MSE of black image after normalisation", black_mse) # 1.0636e-06 - low because most of MNIST is black
 
     # print out white image at all times
     white_seq = torch.full_like(data, 1)
     white_mse = mse_loss(white_seq, data)
-    white_mse = white_mse/(64 * 64 * 10)
     print(white_mse) # 2.3067e-05
 
     rand_seq = torch.rand(data.size(0), data.size(1), data.size(2), data.size(3), data.size(4))
     # print(rand_seq.shape)
     # print(torch.max(rand_seq))
     # print(torch.min(rand_seq))
-    rand_mse = mse_loss(rand_seq, data)/(64 * 64 * 10)
+    rand_mse = mse_loss(rand_seq, data)
     print(rand_mse) # 7.9787e-06
 
     break
